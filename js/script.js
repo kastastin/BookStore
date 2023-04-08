40f2fb8a1c7e0b8e73c62076bf5841b83a7777de
kastastin <kostya242421@gmail.com>
2023-04-09 01:51:57 +0300

    Icons Section (Grid)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,51 @@
 const secondHeader = document.querySelector(".header-2");
+const secondHeaderWhite = document.querySelector(".header-2-white");
+const home = document.querySelector(".home");
 const searchForm = document.querySelector(".search-form");
 const searchBtn = document.querySelector("#search-btn");
 const loginForm = document.querySelector(".login-form-container");
 
 const toggleActiveHeader = function () {
-  window.scrollY > 80
-    ? secondHeader.classList.add("active")
-    : secondHeader.classList.remove("active");
+  if (window.scrollY > 80) {
+    secondHeaderWhite.style.height = "5rem";
+    setTimeout(() => {
+      secondHeader.classList.add("active");
+    }, 1);
+  } else {
+    secondHeaderWhite.style.height = "0rem";
+    secondHeader.classList.remove("active");
+  }
 };
 
 const changeModalVisibility = function () {
   loginForm.classList.toggle("active");
   overlay.classList.toggle("hidden");
 };
+
+// <-- Events -->
+window.addEventListener("scroll", function () {
+  toggleActiveHeader();
+  searchForm.classList.remove("active");
+});
+
+window.addEventListener("load", function () {
+  toggleActiveHeader();
+});
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+});
+
+document.addEventListener("click", function (e) {
+  const classNames = ["fa-user", "fa-times", "active"];
+  if (classNames.some((className) => e.target.classList.contains(className)))
+    loginForm.classList.toggle("active");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && loginForm.classList.contains("active"))
+    loginForm.classList.toggle("active");
+});
 
 // <-- Swiper -->
 const swiper = new Swiper(".books-slider", {
@@ -33,26 +66,4 @@ const swiper = new Swiper(".books-slider", {
       slidesPerView: 3,
     },
   },
-});
-
-// <-- Events -->
-window.addEventListener("scroll", function () {
-  searchForm.classList.remove("active");
-});
-
-window.addEventListener("load", toggleActiveHeader);
-
-searchBtn.addEventListener("click", function () {
-  searchForm.classList.toggle("active");
-});
-
-document.addEventListener("click", function (e) {
-  const classNames = ["fa-user", "fa-times", "active"];
-  if (classNames.some((className) => e.target.classList.contains(className)))
-    loginForm.classList.toggle("active");
-});
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && loginForm.classList.contains("active"))
-    loginForm.classList.toggle("active");
 });
