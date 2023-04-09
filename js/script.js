cfd91c66fca352aa96edae5e6af3e3eefe8485d3
kastastin <kostya242421@gmail.com>
2023-04-09 20:01:16 +0300

    Blog Section

--- js/script.js
+++ js/script.js
@@ -138,3 +138,25 @@ const reviewsSwiper = new Swiper(".reviews-slider", {
     },
   },
 });
+
+const blogsSwiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
