b496cc23dfe21ae871e9ce955314b221047777c7
kastastin <kostya242421@gmail.com>
2023-04-09 19:30:25 +0300

    Review Section

--- js/script.js
+++ js/script.js
@@ -116,3 +116,25 @@ const arrivalsSwiper = new Swiper(".arrivals-slider", {
     },
   },
 });
+
+const reviewsSwiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
